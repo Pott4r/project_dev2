b3fd452de4913f329d0a8db6aaf32a83387f47d3
Pott4r <43205420+Pott4r@users.noreply.github.com>
2019-10-30 16:30:37 +0200

    Add files via upload

---
+++ project/game-source-code/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+//#include <iostream.h>
+//#include <studio.h>
+//#include <string.h>
+
+using namespace std;
+
+class players_cannons
+{
+	public:
+	
+	
+	private:
+	
+};
+
+class aliens_armada
+{
+	public:
+
+	private:
+};
+
+
+class laser_shots          
+{
+	public:
+	public:
+};
+
+
+int main(){
+	
+	players_cannons player1, player2;         //object for player 1 and 2 shots, etc.    
+	
+	aliens_armada aliens1, aliens2;
+	
+	laser_shots player1_shots, player2_shots;
+	
+
+	getX();             //get functions for coordinates..
+	getY();
+	
+	
+	return 0;
+}
+
